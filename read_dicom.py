cdc65a0a1c27c2495437168ce57ca42370c6e911
VigneshKarthikN <33036621+VigneshKarthikN@users.noreply.github.com>
2019-01-12 17:32:24 +0530

    Update read_dicom.py

--- read_dicom.py
+++ read_dicom.py
@@ -4,7 +4,6 @@ def extract(fname):
     data = d.read_file(fname)
     for key in data.keys():
         val = data[key].value
-        print(val)
         output = open('output.txt', 'a')
         output.write(str(val))
         output.write(str('\n'))
